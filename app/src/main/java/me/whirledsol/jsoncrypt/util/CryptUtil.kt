a975bc408f6e428ae31407b3a38e8eb15c5ff58e
whirledsol <astro491@gmail.com>
2022-10-09 13:29:16 -0400

    testing solution to encryption using streams alone

--- app/src/main/java/me/whirledsol/jsoncrypt/util/CryptUtil.kt
+++ app/src/main/java/me/whirledsol/jsoncrypt/util/CryptUtil.kt
@@ -2,14 +2,20 @@ package me.whirledsol.jsoncrypt.util
 
 import android.content.Context
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
+import androidx.annotation.RequiresApi
 import androidx.security.crypto.EncryptedFile
 import androidx.security.crypto.MasterKey
 import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.FileOutputStream
 import java.nio.charset.StandardCharsets
-
-
+import java.util.*
+import javax.crypto.Cipher
+import javax.crypto.CipherOutputStream
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 
 
 /**
@@ -74,6 +80,34 @@ class CryptUtil(var _context: Context) {
         return String(byteArrayOutputStream.toByteArray())
     }
 
+    /**
+     * encryptStream: Guarenteed to work with any URI
+     */
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun encryptStream(uri: Uri, password: String): File{
+        val contentResolver = _context.contentResolver;
+        val externalDir = _context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        val encFileName : String = uri.fragment + ".enc.json"
+        val encryptedFile = File(externalDir,encFileName)
+
+        //key
+        val decodedKey: ByteArray = Base64.getDecoder().decode(password)
+        val secretKey: SecretKey = SecretKeySpec(decodedKey, 0, decodedKey.size, "AES")
+
+        //cipher
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+
+        //stream
+        val inputStream = contentResolver.openInputStream(uri)
+        val outputStream = FileOutputStream(encryptedFile)
+        val cipherOuputStream = CipherOutputStream(outputStream, cipher)
+        inputStream?.copyTo(cipherOuputStream)
+        cipherOuputStream.flush()
+        cipherOuputStream.close()
+
+        return encryptedFile
+    }
     /**
      * encryptFile
      */
---
+++ app/src/main/java/me/whirledsol/jsoncrypt/util/FileUtil.kt
@@ -0,0 +1,36 @@
+package me.whirledsol.jsoncrypt.util
+
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.provider.OpenableColumns
+import java.io.File
+
+class FileUtil(var _context: Context) {
+
+    @SuppressLint("Range")
+    fun resolveFilenameFromUri(uri: Uri): String? {
+
+        val uriString = uri.toString();
+        val myFile = File(uriString);
+        val path = myFile.getAbsolutePath();
+        var displayName : String? = null;
+
+        if (uriString.startsWith("content://")) {
+            var cursor: Cursor? = null;
+            try {
+                cursor = _context.getContentResolver().query(uri, null, null, null, null)!!
+                if (cursor != null && cursor.moveToFirst()) {
+                    displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor?.close();
+            }
+        } else if (uriString.startsWith("file://")) {
+            displayName = myFile.getName();
+        }
+        return displayName;
+    }
+}
